a66e5b7e61027cf22d36a4f6e51408be929435df
Sheliann Olsson <sheliannohlsson@yahoo.com>
2020-09-03 16:25:53 +0200

    initial commit

---
+++ README.md
@@ -0,0 +1,7 @@
+# A modern website made with :
+
+## React Hooks
+
+## React Router
+
+## Viewable link for website :
---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+<h1>Welcome Animal Lovers</h1>
+       
+    </div>
+  );
+}
+
+export default App;
