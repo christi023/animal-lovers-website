a7db72f1aa9266db753f89b85524d59b81c75a1f
Sheliann Olsson <sheliannohlsson@yahoo.com>
2020-09-08 20:38:48 +0200

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,12 +1,21 @@
 import React from 'react';
-
+import { Switch, Route } from 'react-router-dom';
 import './App.css';
+// components
+import Navbar from './Components/Navbar/Navbar';
+import Home from './Components/Pages/Home/Home';
+//import Services from './Components/Pages/Services/Services';
+//import Products from './Components/Pages/Products/Products';
+//import SignUp from './Components/Pages/SignUp/SignUp';
+//import Footer from './Components/Pages/Footer/Footer';
 
 function App() {
   return (
     <div className="App">
-<h1>Welcome Animal Lovers</h1>
-       
+      <Navbar />
+      <Switch>
+        <Route exact path="/" component={Home} />
+      </Switch>
     </div>
   );
 }
