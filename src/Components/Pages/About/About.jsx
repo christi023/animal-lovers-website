771e4ba4301130c89fb41734250801787ab5018a
Sheliann Olsson <sheliannohlsson@yahoo.com>
2020-09-09 20:10:09 +0200

    created About Page

---
+++ src/Components/Pages/About/About.jsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import Common from '../../Common';
+import web from '../../../assets/caregiver.jpeg';
+import web1 from '../../../assets/caregiver1.jpeg';
+import web2 from '../../../assets/caregiver2.jpeg';
+// style
+import './About.css';
+
+export default function About() {
+  return (
+    <>
+      <Common
+        name="About "
+        imgSrc={web}
+        imgSrc1={web1}
+        imgSrc2={web2}
+        visit="/contact"
+        btName="Contact Now"
+        imgStyle={{ borderRadius: '80px' }}
+      />
+    </>
+  );
+}
