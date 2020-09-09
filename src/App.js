fec974c83b897f2eb35d90f41627952c796dbb0a
Sheliann Olsson <sheliannohlsson@yahoo.com>
2020-09-09 20:16:21 +0200

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,22 +1,30 @@
 import React from 'react';
-import { Switch, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import ScrollToTop from './Components/ScrollToTop';
 import './App.css';
 // components
 import Navbar from './Components/Navbar/Navbar';
 import Home from './Components/Pages/Home/Home';
-//import Services from './Components/Pages/Services/Services';
-//import Products from './Components/Pages/Products/Products';
+import About from './Components/Pages/About/About';
+import Contact from './Components/Pages/Contact/Contact';
+import Services from './Components/Pages/Services/Services';
+import Team from './Components/Pages/Team/Team';
 //import SignUp from './Components/Pages/SignUp/SignUp';
 //import Footer from './Components/Pages/Footer/Footer';
 
 function App() {
   return (
-    <div className="App">
+    <Router>
+      <ScrollToTop />
       <Navbar />
       <Switch>
         <Route exact path="/" component={Home} />
+        <Route exact path="/about" component={About} />
+        <Route exact path="/contact" component={Contact} />
+        <Route exact path="/services" component={Services} />
+        <Route exact path="/team" component={Team} />
       </Switch>
-    </div>
+    </Router>
   );
 }
 
--- src/Components/Pages/Home/Home.jsx
+++ src/Components/Pages/Home/Home.jsx
@@ -1,11 +1,17 @@
 import React from 'react';
 // Components
 import Carrousel from '../../Carousel/Carrousel';
-
+import About from '../../Pages/About/About';
+import Services from '../../Pages/Services/Services';
+import Team from '../../Pages/Team/Team';
+//import Contact from '../../Pages/Contact/Contact';
 export default function Home() {
   return (
     <>
       <Carrousel />
+      <About />
+      <Services />
+      <Team />
     </>
   );
 }
