978b6f1385fa33ed964c96f0a2f30b17f2331e7b
Sheliann Olsson <sheliannohlsson@yahoo.com>
2020-09-21 21:27:23 +0200

    added sign in & register form

--- src/Components/Register_Login/Login.jsx
+++ src/Components/Register_Login/Login.jsx
@@ -1,9 +1,35 @@
 import React from 'react';
+import { useForm } from 'react-hook-form';
 
 export default function Login() {
+  const { register, handleSubmit, reset, errors } = useForm();
+  const onSubmit = (data, e) => {
+    e.target.reset(); // reset after form submit
+  };
+
+  console.log(errors);
+
   return (
-    <div>
-      <h1>Login</h1>
-    </div>
+    <form onSubmit={handleSubmit(onSubmit)}>
+      <label>First name</label>
+      <input type="text" name="firstName" ref={register({ required: true })} />
+      {errors.firstName && <p>This is required</p>}
+
+      <label>Last name</label>
+      <input type="text" name="lastName" ref={register} />
+
+      <input type="submit" />
+      <input
+        style={{ display: 'block', marginTop: 20 }}
+        type="reset"
+        value="Standard Reset Field Values"
+      />
+      <input
+        style={{ display: 'block', marginTop: 20 }}
+        type="button"
+        onClick={() => reset()}
+        value="Custom Reset Field Values & Errors"
+      />
+    </form>
   );
 }
--- src/Components/Register_Login/Register.jsx
+++ src/Components/Register_Login/Register.jsx
@@ -1,11 +1,37 @@
 import React from 'react';
+import { useForm } from 'react-hook-form';
 // style
 import './Register_Login.css';
 
 export default function Register() {
+  const { register, handleSubmit, reset, errors } = useForm();
+  const onSubmit = (data, e) => {
+    e.target.reset(); // reset after form submit
+  };
+
+  console.log(errors);
+
   return (
-    <div>
-      <h1>Sign Up</h1>
-    </div>
+    <form className="" onSubmit={handleSubmit(onSubmit)}>
+      <label>First name</label>
+      <input type="text" name="firstName" ref={register({ required: true })} />
+      {errors.firstName && <span>This is required</span>}
+
+      <label>Last name</label>
+      <input type="text" name="lastName" ref={register} />
+
+      <input type="submit" />
+      <input
+        style={{ display: 'block', marginTop: 20 }}
+        type="reset"
+        value="Standard Reset Field Values"
+      />
+      <input
+        style={{ display: 'block', marginTop: 20 }}
+        type="button"
+        onClick={() => reset()}
+        value="Custom Reset Field Values & Errors"
+      />
+    </form>
   );
 }
