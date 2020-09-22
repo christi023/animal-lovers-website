8af7e775ae40e60d727ac305825809e65839e0c6
Sheliann Olsson <sheliannohlsson@yahoo.com>
2020-09-22 21:43:06 +0200

    added sign in & register form

--- src/Components/Register_Login/Register.jsx
+++ src/Components/Register_Login/Register.jsx
@@ -4,7 +4,7 @@ import { useForm } from 'react-hook-form';
 import './Register_Login.css';
 
 export default function Register() {
-  const { register, handleSubmit, reset, errors } = useForm();
+  const { register, handleSubmit, errors } = useForm();
   const onSubmit = (data, e) => {
     e.target.reset(); // reset after form submit
   };
@@ -12,26 +12,18 @@ export default function Register() {
   console.log(errors);
 
   return (
-    <form className="" onSubmit={handleSubmit(onSubmit)}>
-      <label>First name</label>
-      <input type="text" name="firstName" ref={register({ required: true })} />
-      {errors.firstName && <span>This is required</span>}
-
-      <label>Last name</label>
-      <input type="text" name="lastName" ref={register} />
-
-      <input type="submit" />
-      <input
-        style={{ display: 'block', marginTop: 20 }}
-        type="reset"
-        value="Standard Reset Field Values"
-      />
-      <input
-        style={{ display: 'block', marginTop: 20 }}
-        type="button"
-        onClick={() => reset()}
-        value="Custom Reset Field Values & Errors"
-      />
-    </form>
+    <>
+      <div className="form-display">
+        <form className="" onSubmit={handleSubmit(onSubmit)}>
+          <label>First name</label>
+          <input type="text" name="firstName" ref={register({ required: true })} />
+          {errors.firstName && <span>This is required</span>}
+          <label>Last name</label>
+          <input type="text" name="lastName" ref={register} />
+          {errors.lastName && <span>This is required</span>}
+          <input type="submit" />
+        </form>
+      </div>
+    </>
   );
 }
