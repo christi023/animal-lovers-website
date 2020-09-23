930bbf04ffd43f1c01b76bc7a842b20be5e098b4
Sheliann Olsson <sheliannohlsson@yahoo.com>
2020-09-23 18:40:35 +0200

    react hooks register form with extended functionality

--- src/Components/Register_Login/Register.jsx
+++ src/Components/Register_Login/Register.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
+import { NavLink } from 'react-router-dom';
 import { useForm } from 'react-hook-form';
 // style
 import './Register_Login.css';
 
 export default function Register() {
   const { register, handleSubmit, errors } = useForm();
+
   const onSubmit = (data, e) => {
     e.target.reset(); // reset after form submit
   };
@@ -16,14 +18,59 @@ export default function Register() {
       <div className="form-display">
         <form className="" onSubmit={handleSubmit(onSubmit)}>
           <label>First name</label>
-          <input type="text" name="firstName" ref={register({ required: true })} />
+          <input
+            type="text"
+            name="firstName"
+            placeholder="First Name"
+            ref={register({ required: true })}
+          />
+
           {errors.firstName && <span>This is required</span>}
           <label>Last name</label>
-          <input type="text" name="lastName" ref={register} />
-          {errors.lastName && <span>This is required</span>}
+          <input
+            type="text"
+            name="lastName"
+            placeholder="Last Name"
+            ref={register({ required: true })}
+          />
+
+          <label>Email</label>
+          {errors.email && <span>This is required</span>}
+          <input
+            type="email"
+            name="email"
+            placeholder="info@example.com"
+            ref={register({ required: true })}
+          />
+
+          <label>Password</label>
+          {errors.password && <span>This is required</span>}
+          <input
+            type="password"
+            name="password"
+            placeholder="Enter password"
+            ref={register({ required: true })}
+          />
+          {errors.password && <span>This is required</span>}
+
+          <label>Confirm Password</label>
+          {errors.password && <span>This is required</span>}
+          <input
+            type="password"
+            name="password1"
+            placeholder="Enter password"
+            ref={register({ required: true })}
+          />
+          {errors.password1 && <span>This is required</span>}
+
           <input type="submit" />
         </form>
+        <h6 className="form-input-login">
+          Already have an account? Login <NavLink to="/login">Click</NavLink>
+        </h6>
       </div>
+      <br />
+      <br />
     </>
   );
 }
